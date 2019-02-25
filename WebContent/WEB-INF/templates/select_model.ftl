43568049988afcd98b559a95f1326fee1165ca9c
HoussemCHEMINGUI <houss.Chemingui@gmail.com>
2019-02-25 17:40:10 +0100

    NEW: first upload

---
+++ WebContent/WEB-INF/templates/select_model.ftl
@@ -0,0 +1,61 @@
+<#if hasError>
+	<p><span class="errorMessage">ERROR: ${errorMessage}.</span></p>
+	<p><a href="javascript:history.back()">Back</a></p>
+<#else>
+	<script type="text/javascript">
+	<!--
+	 function sortModels(sortBy)
+	{
+		ajax_loadContent('_model_repository','/SPLOT/SplotAnalysesServlet?action=select_model&enableSelection=${enableSelection?string}&selectionMode=${selectionMode}&serviceURL=${serviceURL}&serviceHTTPMethod=${serviceHTTPMethod}&serviceAction=${serviceAction}&sortby='+sortBy);
+	}
+	-->
+	</script>
+	<div id="_model_repository">
+		<#compress>
+		<#if enableSelection>
+			<#if selectionMode="multiple">
+			  <form action="${serviceURL}" method="${serviceHTTPMethod}">
+			  <input type="hidden" name="action" value="${serviceAction}">
+			  <b>Please, type the URL of your own feature models AND/OR select models from the table below and</b>
+			  <input class="standardHighlight1" type="submit" value="Click Here"/>
+			  <p style="margin: 10px 0px 0px 0px">Your model's URLs (in the <a href="sxfm.html">SXFM format</a>) starting with "http://" (one per line):</p>
+			  <p style="margin: 0px 0px 10px 0px"><textarea name="userModels" rows=3 cols="80"></textarea></p>
+			<#elseif selectionMode="single">
+			  <form action="${serviceURL}" method="${serviceHTTPMethod}">
+			  <input type="hidden" name="action" value="${serviceAction}">
+			  <input type="hidden" name="op" value="reset">
+			  <b>Select a product line model from the table below and</b>
+			  <input class="standardHighlight1" type="submit" value="Click Here"/>
+			 
+			</#if>
+		</#if>
+		</#compress>
+<br/><br/>
+		<table class="standardTableStyle">
+		<tr>
+			<th>#</th>
+			<th><#if sortBy!='name'><a href="javascript:sortModels('name');">Name</a><#else><span class="standardHighlight1">Name</a></#if></TH>
+			<th><#if sortBy!='features'><a href="javascript:sortModels('features');">Variants</a><#else><span class="standardHighlight1">Variants</a></#if></TH>
+			<th><#if sortBy!='creator'><a href="javascript:sortModels('creator');">Creator</a><#else><span class="standardHighlight1">Creator</a></#if></TH>
+			<th>Creation Date</th>
+			
+		</tr>
+		<#list models as model>
+		<tr onmouseover="this.className='highlightFMRepositoryEntry';" onmouseout="this.className='standardTableStyle';">
+			<td>${model_index+1}.</td>
+			<TD>
+			<#if enableSelection><input class="standardHighlight1" <#if selectionMode="multiple"> type="checkbox"  <#elseif selectionMode="single"> type="radio" </#if> name="selectedModels" value="${model.file}"/></#if>${model.name}
+			</td>
+			<TD>${model.features}</td>
+			
+			<TD>${model.creator}&nbsp;</td>
+			<TD>${model.date}&nbsp;</td>
+			
+		</tr>
+		</#list>
+		</table>
+		<#if enableSelection>
+			</form>
+		</#if>
+	</div>
+</#if>
---
+++ src/splot/services/handlers/conf/InteractiveConfigurationExportConfigurationHandler.java
@@ -0,0 +1,119 @@
+package splot.services.handlers.conf;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import splar.core.fm.FeatureGroup;
+import splar.core.fm.FeatureModel;
+import splar.core.fm.FeatureTreeNode;
+import splar.core.fm.GroupedFeature;
+import splar.core.fm.SolitaireFeature;
+import splar.core.fm.configuration.ConfigurationEngine;
+import splot.core.FreeMarkerHandler;
+import splot.core.HandlerExecutionException;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class InteractiveConfigurationExportConfigurationHandler extends FreeMarkerHandler {
+
+	public InteractiveConfigurationExportConfigurationHandler(String handlerName, HttpServlet servlet, Configuration configuration, Template template) {
+		super(handlerName, servlet, configuration, template);
+	}
+	
+	public void buildModel(HttpServletRequest request, HttpServletResponse response, Map templateModel) throws HandlerExecutionException {
+
+        try {	        		        	
+        	HttpSession session = request.getSession(true);	        	
+
+        	ConfigurationEngine confEngine = (ConfigurationEngine)session.getAttribute("conf_engine");
+        	FeatureModel model = confEngine.getModel();
+        	Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            sdf.format(cal.getTime());
+    		if (confEngine == null) {
+    			throw new HandlerExecutionException("Configuration engine must be created first");
+    		}
+        	
+        	templateModel.put("modelName", model.getName());
+        	templateModel.put("OriginatorName1", "Houssem");
+
+        	List features = new LinkedList();
+        	for( FeatureTreeNode featureNode : model.getNodes() ) {
+        		if ( featureNode.isInstantiated() ) {
+	        		Map featureData = new HashMap();
+	    			featureData.put("id", featureNode.getID());
+	    			featureData.put("name", getFeatureName(featureNode));
+	    			featureData.put("type", getFeatureType(featureNode));
+	    			featureData.put("value", ""+featureNode.getValue());
+	    			featureData.put("decisionType", featureNode.getValue() == -1 ? "" : (String)featureNode.getProperty("decisionType"));   // manual, propagated, auto-completion
+	    			featureData.put("decisionStep", featureNode.getValue() == -1 ? "" : (String)featureNode.getProperty("decisionStep"));   
+	    			//featureData.put("timestamp", sdf.format(cal.getTime()));
+	    			//  NEW
+	    			featureData.put("timeStamp",  featureNode.getProperty("timeStamp") == null ? "None" :featureNode.getProperty("timeStamp"));
+	    			features.add(featureData);
+        		}
+        	}
+        	templateModel.put("features", features);	        	
+	        	
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new HandlerExecutionException("Configuration engine must be created first", e);
+		}
+	}
+ protected LocalTime getFeatureTime( FeatureTreeNode feature ) {
+		
+	return LocalTime.now();
+	}
+	
+	
+	protected String getFeatureParent(FeatureTreeNode feature) {
+		FeatureTreeNode parent = (FeatureTreeNode)feature.getParent();
+		if ( parent == null ) {
+			return "";
+		}
+		return parent.getID();		
+	}
+	
+	protected String getFeatureName( FeatureTreeNode feature ) {
+		if ( feature instanceof FeatureGroup ) {
+			int min = ((FeatureGroup)feature).getMin();
+			int max = ((FeatureGroup)feature).getMax();
+			max = ( max==-1 ) ? feature.getChildCount() : max; 
+			return "[" + min + ".." + max +"]";
+		}
+		return feature.getName();
+	}
+	
+	protected String getFeatureType( FeatureTreeNode feature ) {
+		if ( feature.isRoot() ) { 
+			return "templateModel";				
+		}
+		else if ( feature instanceof SolitaireFeature) {
+			if (((SolitaireFeature)feature).isOptional()) {
+				return "optional";
+			}
+			else {
+				return "mandatory";
+			}
+		}
+		else if ( feature instanceof FeatureGroup ){
+			return "group";
+		}
+		else if ( feature instanceof GroupedFeature ){
+			return "grouped";
+		}
+		return "error";
+	}	
+}
+
